daad6b2d19b7117775b22aaaeb12375ed276d808
Davide <davide017@hotmail.it>
2025-05-28 14:02:01 +0200

    Aggiornato a 28/05/25 - add portfolio link cv

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
       <ul className="space-y-2 text-sm flex flex-col items-center border-t border-border pt-3">
         <li className="flex items-center gap-2">
           <Mail className="w-4 h-4 text-blue-500" />
-          <a href={`mailto:${email}`} className="text-sm text-text hover:text-primary transition">
+          <a href={`mailto:${email}`} className="text-sm text-text hover:text-emerald-300 transition">
             {email}
           </a>
         </li>
@@ -23,21 +23,25 @@ const Contact = () => {
             href={`https://github.com/${github}`}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-sm text-text hover:text-primary transition"
+            className="text-sm text-text hover:text-emerald-300 transition"
           >
             @{github}
           </a>
         </li>
-        <li className="flex items-center">
-          <Globe className="w-4 h-4 text-indigo-500" />
+        <li className="flex flex-col items-center text-sm">
           <a
             href={portfolio}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-sm text-text hover:text-primary transition"
+            className="flex flex-col items-center text-text hover:text-emerald-300 transition text-center"
           >
-            <div>Portfolio :</div>
-            <span>davide-martinico-portfolio.netlify.app/</span>
+            <div className="flex items-center gap-2">
+              <Globe className="w-4 h-4 text-indigo-500" />
+              <span>Portfolio:</span>
+            </div>
+            <div className="mt-1">
+              davide-martinico-portfolio.netlify.app
+            </div>
           </a>
         </li>
       </ul>
